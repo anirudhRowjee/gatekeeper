429eb955114c8ed272d2e5e1767e4381682d2ceb
Shreyas <shreyasdev08@gmail.com>
2019-11-17 22:14:04 +0530

    frontend update #2

--- gatekeeper_api/student/views.py
+++ gatekeeper_api/student/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    # filler
-    return render(request, 'student/addstudents.html')
 
+def home(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "student/addstudents.html")
