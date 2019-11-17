1855d112a784c6a5df124bd9613ca6e36b456944
Anirudh Rowjee <ani.rowjee@gmail.com>
2019-11-17 17:46:56 +0530

    dummy urls

--- gatekeeper_api/gatekeeper_api/urls.py
+++ gatekeeper_api/gatekeeper_api/urls.py
@@ -6,6 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('passes/', include('passes.urls')),
+    path('students/', include('student.urls'), name='students_home'),
+    path('utils/', include('utils.urls'), name='utils_urls'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
